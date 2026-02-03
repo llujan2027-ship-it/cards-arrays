b8383f1ebc5440bdb01ca521a2d2f058d3125036
llujan2027-ship-it <llujan2027@cchsdons.com>
2026-02-03 12:14:51 -0800

    Add files via upload

---
+++ CardsArrays.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cardsarrays;
+
+/**
+ *
+ * @author llujan2027
+ */
+public class CardsArrays {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
