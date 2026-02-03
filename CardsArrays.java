8bbb604f3734344e3dbb69750dfee334e5a8853b
llujan2027-ship-it <llujan2027@cchsdons.com>
2026-02-03 12:15:32 -0800

    Add files via upload

---
+++ CardsArrays.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cardsarrays;
+
+/**
+ *
+ * @author llujan2027
+ */
+public class CardsArrays {
+
+    public static void main(String[] args) {
+        //deck[suit][rank]
+        String[][] deck = new String[4][13];
+        
+        for(int suit = 0; suit < deck.length; suit++){
+            for(int rank = 0; rank <deck[rank].length; rank++){
+                
+            }
+        }
+    }
+}
